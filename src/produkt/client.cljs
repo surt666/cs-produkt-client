a9cde34bb0464bf80a4c9e5429ca5e63c7730ac2
Steen Larsen <steen666@gmail.com>
2011-08-09 12:45:53 +0200

    button events working

--- src/produkt/client.cljs
+++ src/produkt/client.cljs
@@ -1,8 +1,20 @@
 (ns produkt.client
  (:require [produkt.ajax :as ajax]
-           [goog.dom :as dom]))
+           [goog.dom :as dom]
+           [goog.events.Event :as goog-event]
+           [goog.events.EventType :as goog-event-type]
+           [goog.ui.CustomButton :as custom-button]
+           [goog.ui.Component.EventType :as comp-event-type]))
 
 (def base-url "http://localhost:8080/produkt/")
+
+(def opret-button (doto (goog.ui.CustomButton.)
+                        (.setTooltip "Opret produkt")
+                        (.setCaption "Opret")))
+
+(def opret-meta-button (doto (goog.ui.CustomButton.)
+                        (.setTooltip "Opret Metadata værdi")
+                        (.setCaption "Opret Meta")))
 
 (defn- generate-options [data]
   (reduce str (map #(str "<option id=\"" (:id %) "\">" (:navn %) "</option>") data)))
@@ -19,4 +31,18 @@
   (produkt.ajax/get-uri (str base-url "findalle/services") (partial select-data "services"))
   (produkt.ajax/get-uri (str base-url "findalle/produkter") (partial select-data "produkter")))
 
-(doajax)
+(defn event1 []
+  ((js* "alert") "BLA"))
+
+(defn ^:export main [] 
+ ;(doajax)
+  (.render opret-button (dom/getElement  "submit_p"))
+  (.listen goog.events
+           opret-button
+           goog.ui.Component.EventType/ACTION
+           event1)
+  (.render opret-meta-button (dom/getElement  "submit_m"))
+  (.listen goog.events
+           opret-meta-button
+           goog.ui.Component.EventType/ACTION
+           event1))
